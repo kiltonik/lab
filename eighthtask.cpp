c6b0f9746e0a62cae6abf1ffff028d314b4cadeb
kiltonik <valkovmd@mail.ru>
2021-03-22 21:53:29 +0300

    Added some logic to eighth task UI

--- eighthtask.cpp
+++ eighthtask.cpp
@@ -1,5 +1,8 @@
 #include "eighthtask.h"
 #include "ui_eighthtask.h"
+#include "regex"
+#include <QMessageBox>
+
 
 EighthTask::EighthTask(QWidget *parent) :
     QDialog(parent),
@@ -13,14 +16,16 @@ EighthTask::~EighthTask()
     delete ui;
 }
 
-//Метод будет закидывать в разные списки студентов на экране
+//В этом методе пропиши вызовы свои функций
+//для распределения элементов по двум спискам на экране
+// Метод вызывается при нажатии на кнопку
 void EighthTask::on_show_students_presence_clicked()
 {
-    //Метод ниже добавляет студентов, у которых >= 8
+    //Метод ниже добавляет студента, у которого >= 8 в нужный список
     //туда надо подставить QString
 //    ui->students_all_present->addItem();
 
-    //Метод добавляет студентов у кого меньше 8
+    //Метод добавляет студента у которого меньше 8 в нужный список
     //Аналогично нужен QString
 //    ui->students_some_present->addItem();
 }
@@ -37,7 +42,16 @@ void EighthTask::on_clear_data_clicked()
 void EighthTask::on_add_presence_clicked()
 {
     QString item = ui->presence_edit->text(); //Введенная информация в формате день, студент.
-    //Другие форматы я отсеку потом
-
-    ui->presence_list->addItem(item);
+    if(std::regex_match(
+                item.begin(),
+                item.end(),
+                std::regex("\\d{1,2}, [A-Z][a-z]+"))){
+        ui->presence_list->addItem(item);
+        //Пропиши тут логику сохранения
+    }
+    else{
+        QMessageBox error(this);
+        error.setText("Wrong input format");
+        error.exec();
+    }
 }
